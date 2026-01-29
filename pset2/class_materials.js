14e74c54c05e75bc8c49734522bb6912459d14a3
icebearisicebear <pzhang3@berklee.edu>
2026-01-29 16:50:45 -0500

    commit

---
+++ pset2/class_materials.js
@@ -0,0 +1,21 @@
+/*
+let semester = 5;
+print(semester);
+
+semester = 6;
+print(semester);
+
+const major = 'performance';
+print(major);
+*/
+
+let firstName = 'Peter';
+
+let nameUC = firstName.toUpperCase();
+print(nameUC);
+
+let nameLC = firstName.toLowerCase();
+print(nameLC);
+
+let nameLength = firstName.length;
+print(nameLength);
--- pset2/variables.js
+++
@@ -1,5 +0,0 @@
-let semester = 5;
-print(semester);
-
-semester = 6;
-print(semester);
