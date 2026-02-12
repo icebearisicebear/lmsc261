1e3f717ffb1263a603a35dc4f0bde7cf25038074
icebearisicebear <pzhang3@berklee.edu>
2026-02-12 16:12:27 -0500

    Update icecream2.js

--- pset3/icecream2.js
+++ pset3/icecream2.js
@@ -1,7 +1,7 @@
 const conesSoldPerHour = 14;
 const inventory = 333333333333;
 
-for (let i = 0; i < 12; i++) {
+for (let i = 1; i <= 12; i++) {
     print(conesSoldPerHour * i + ' cones sold at hour ' + i + '.');
     print(inventory - (conesSoldPerHour * i) + ' left.');
 }
