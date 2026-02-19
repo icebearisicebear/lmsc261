9c7631175566a3d8e059f7d1d9747e8c8aac414e
icebearisicebear <pzhang3@berklee.edu>
2026-02-19 17:23:30 -0500

    Update functions.js

--- classwork/week5/functions.js
+++ classwork/week5/functions.js
@@ -1,5 +1,17 @@
+/*
 function greet(phrase, nameToPrint){
 print(phrase + ', ' + nameToPrint + '!');
 }
 
 greet('Hello', 'Peter'); 
+*/
+
+function square(numberToSquare){
+    let squaredNumber = numberToSquare * numberToSquare;
+    return squaredNumber
+}
+print(
+    square(
+        prompt('Number to square: ')
+    )
+)
