5aaf3694519e47046c00af27e5a516e2a905ad11
icebearisicebear <pzhang3@berklee.edu>
2026-02-19 17:46:51 -0500

    Update functions.js

--- classwork/week5/functions.js
+++ classwork/week5/functions.js
@@ -6,6 +6,7 @@ print(phrase + ', ' + nameToPrint + '!');
 greet('Hello', 'Peter'); 
 */
 
+/*
 function square(numberToSquare){
     let squaredNumber = numberToSquare * numberToSquare;
     return squaredNumber
@@ -15,3 +16,15 @@ print(
         prompt('Number to square: ')
     )
 )
+*/
+
+
+function cakeDivision(numOfAttendees){
+    if (numOfAttendees === 0){
+        return 6.28;
+    }
+    let cakeInRadian = 6.28;
+    return cakeInRadian / numOfAttendees;
+}
+
+print(cakeDivision(prompt('How many people?')))
